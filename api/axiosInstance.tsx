cc055145f6fd1fb8acdfd00452eceebc1f182491
homin <ghals5737@naver.com>
2023-01-25 21:32:45 +0900

    [fix]:api http 프로토콜 수정

--- api/axiosInstance.tsx
+++ api/axiosInstance.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 const instance = axios.create({
-    baseURL: 'http://3.38.30.106:8080/api',
+    baseURL: 'https://3.38.30.106:8080/api',
     timeout: 1000,
     headers: {'X-Custom-Header': 'foobar'},
     withCredentials: true
