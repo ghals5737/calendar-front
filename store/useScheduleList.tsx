1946f1b76bf30543945094e06315ae4383aaad94
homin <ghals5737@naver.com>
2023-01-28 23:52:17 +0900

    get schedule 추가

--- store/useScheduleList.tsx
+++ store/useScheduleList.tsx
@@ -20,5 +20,5 @@ export const useScheduleList = create<scheduleListInfo>((set) => ({
             }))
         }) 
                 
-    }
+    },    
 }));
--- types/scheduleListInfo.tsx
+++ types/scheduleListInfo.tsx
@@ -3,6 +3,7 @@ import scheduleInfo from './scheduleInfo'
 interface scheduleListInfo{
     scheduleList:scheduleInfo[];
     addSchedule:(schedule:scheduleInfo)=>void
+    getScheduleList:()=>void
 }
 
 export default scheduleListInfo;
