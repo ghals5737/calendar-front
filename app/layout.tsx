232ac2564e47af4885f93a9219f509c438b98c45
ulms <ghals5737@naver.com>
2023-02-21 17:02:13 +0900

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>              
         <link href='//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css' rel='stylesheet' type='text/css'/>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="referrer" content="no-referrer-when-downgrade" />
+        <meta name="referrer" content="no-referrer" />
         <link rel="icon" href="../img/favicon.ico" />
         <title>MINICAL</title>
       </head>
