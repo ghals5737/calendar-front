252bb2ad613bde6c344203e26e49f1beade0486e
ulms <ghals5737@naver.com>
2023-02-22 13:48:59 +0900

    a

--- api/axiosInstance.tsx
+++ api/axiosInstance.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-//axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = true;
 //https://minical.shop/api
 const local='http://localhost:8080/api'
 const prd='https://minical.shop/api'
