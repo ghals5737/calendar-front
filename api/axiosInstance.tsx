75b23fff6745c383c63c44a1d45f0e98c65e2dae
ulms <ghals5737@naver.com>
2023-02-22 13:52:59 +0900

    aa

--- api/axiosInstance.tsx
+++ api/axiosInstance.tsx
@@ -1,12 +1,12 @@
 import axios from "axios";
-axios.defaults.withCredentials = true;
+//axios.defaults.withCredentials = true;
 //https://minical.shop/api
 const local='http://localhost:8080/api'
 const prd='https://minical.shop/api'
 const instance = axios.create({
     baseURL: prd,
     timeout: 1000,    
-    withCredentials: true
+    withCredentials: true,
 });
 
 export default instance
