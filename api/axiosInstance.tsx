936d8c6652291fd1cab4ebfddf24b324a4b2a526
ulms <ghals5737@naver.com>
2023-02-22 15:34:28 +0900

    a

--- api/axiosInstance.tsx
+++ api/axiosInstance.tsx
@@ -6,7 +6,7 @@ const prd='https://minical.shop/api'
 const instance = axios.create({
     baseURL: prd,
     timeout: 10000,    
-    withCredentials: false,
+    withCredentials: true,
 });
 
 export default instance
